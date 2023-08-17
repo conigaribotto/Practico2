0e8c1ef51dc90618cff157f2922e1c455f50158e
FiorellaAilen <fiorellaailen@gmail.com>
2023-08-17 19:14:14 -0300

    Update Main

--- src/testMain/Main.java
+++ src/testMain/Main.java
@@ -7,6 +7,9 @@ public class Main {
         //4_ Desde la java main class, llamar a los metodos de unReloj, accediendo desde objeto unaPersona
         // Llamar los metodos de todas las clasess
         
+        RelojFit relojFit = new RelojFit("Jueves", "19:10hs", "Slim", 1234567, 3,3);
+        
+        Persona persona = new Persona("Paola", "Argento", 18, 1.65, relojFit);
         
     }
 
